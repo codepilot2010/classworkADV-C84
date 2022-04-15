6ddab16413156dcf2ab2725acb891a62e744d69f
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2022-04-15 12:48:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,13 @@ rover_height=90;
 rover_x=10;
 rover_y=10;
 
-background_image="mars.jpg";
+var images_array=["1.JPG","2.JPG","3.JPG","4.jpg","5.jpg"];
+
+random_number=Math.floor(Math.random()*5);
+
+
+background_image=images_array[random_number];
+console.log("background_image="+background_image);
 rover_image="rover.png";
 
 function add()
@@ -18,13 +24,13 @@ function add()
 
     rover_imgtag=new Image();
     rover_imgtag.onload=uploadrover;
-    rover_imgtagsrc=rover_image;
+    rover_imgtag.src=rover_image;
 
 }
 
 function uploadbackground()
 {
-    ctx.drawImage(background_imgtag,0,0,canvas.with,canvas.height);
+    ctx.drawImage(background_imgtag,0,0,canvas.width,canvas.height);
 }
 
 function uploadrover()
@@ -59,4 +65,47 @@ function my_keydown(e)
     }
 
 
+    }
+    function up()
+    {
+      if (rover_y>=0)
+      {
+        rover_y=rover_y-10;
+        console.log("when up arrow is pressed, x="+rover_x+"and y= "+rover_y);
+        uploadbackground();
+        uploadrover();
+      }
+    }
+
+    function down()
+    {
+      if (rover_y<=500)
+      {
+        rover_y=rover_y+10;
+        console.log("when down arrow is pressed, x="+rover_x+"and y= "+rover_y);
+        uploadbackground();
+        uploadrover();
+      }
+    }
+
+    function left()
+    {
+      if (rover_x>=0)
+      {
+        rover_x=rover_x-10;
+        console.log("when left arrow is pressed, x="+rover_x+"and y= "+rover_y);
+        uploadbackground();
+        uploadrover();
+      }
+    }
+
+    function right()
+    {
+      if (rover_x<=700)
+      {
+        rover_x=rover_x+10;
+        console.log("when right arrow is pressed, x="+rover_x+"and y= "+rover_y);
+        uploadbackground();
+        uploadrover();
+      }
     }
